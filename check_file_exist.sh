2c0fe8a6df44be5097d6126b1cc4263ec0fff51c
SaniAdamou14 <sonicstark3@gmail.com>
2025-05-07 12:12:35 +0100

    Correctif7

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -4,9 +4,12 @@
 # Objectif : vérifie si un fichier existe dans le répertoire courant
 # Auteur : Mahamane Sani Adamou Mahamane
 
+# Demander le nom du fichier à l'utilisateur
 echo "Entrez le nom du fichier :"
+# Lire le nom du fichier
 read filename
 
+# Vérifier si le fichier existe
 if [ -f "$filename" ]; then
     echo "Le fichier '$filename' existe."
 else
